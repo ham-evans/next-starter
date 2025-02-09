c130b5aeeb69d131cecd5b33034234e110793e2c
Ham <ham97@mac.com>
2025-02-09 15:37:47 -0500

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import nextjs from '@next/eslint-config-next';
+
+const config = [
+  {
+    extends: [...nextjs],
+  },
+];
+
+export default config;
---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import { GeistSans } from 'geist/font/sans'
+import ThemeProvider from '@/providers/ThemeProvider'
+import NextTopLoader from 'nextjs-toploader'
+import { Analytics } from '@vercel/analytics/react'
+import './globals.css'
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Next.js and Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={GeistSans.className}
+      style={{ colorScheme: 'dark' }}
+      suppressHydrationWarning
+    >
+      <body className="bg-background text-foreground">
+        <NextTopLoader showSpinner={false} height={2} color="#2acf80" />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex min-h-screen flex-col items-center">
+            {children}
+            <Analytics />{' '}
+            {/* ^^ remove this if you are not deploying to vercel. See more at https://vercel.com/docs/analytics  */}
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
